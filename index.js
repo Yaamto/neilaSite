ad35c986566f0a214dd7f4dcd0919ec6193a59cc
yaamto <bilou.bout@gmail.com>
2022-02-16 22:39:16 +0100

    References added, animation and fixed menu + started contact sectionn

--- index.js
+++ index.js
@@ -34,3 +34,33 @@ document.querySelectorAll('#ite').forEach(element => element.addEventListener('c
 );
 
 
+var navigation = document.querySelector('.navigation')
+
+window.onscroll = () => {
+	
+	if(this.scrollY > 300) navigation.classList.add('scroll'); else navigation.classList.remove('scroll')
+  };
+
+  let burgerNav= document.querySelectorAll('.item-nav')
+ let burger=document.getElementById('burger')
+  burgerNav.forEach(item => burger.addEventListener('change', function() {
+	item.classList.toggle('toggle-burger')
+	document.querySelectorAll('#ite').forEach(element => element.addEventListener('click', () => {
+		document.querySelector('#burger').checked= false
+		item.classList.remove('toggle-burger')
+	 })
+	);
+  }))
+ 
+
+//   MODAL
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+function toggleModal(){
+  modalContainer.classList.toggle("active")
+}
+
+
